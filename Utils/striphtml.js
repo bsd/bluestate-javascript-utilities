f0d23376b9a247196c92d3e9c304615bc9c03695
John Paul Davis <john@johnpauldavis.org>
2020-10-01 16:50:07 -0400

    Add ability to allow certain tags

--- Utils/striphtml.js
+++ Utils/striphtml.js
@@ -2,10 +2,15 @@
  * Given an HTML string, eturns a string of plain text
  *
  * @param  {Object} html - string of html to strip
- * @return {String} A plain text string
+ * @param  {String} exceptions - comma-delimted list of tags to allow
+ * @return {String} A plain text string (with allowed tags)
  */
-export function striphtml(html) {
-    const parsed = new DOMParser().parseFromString(html, 'text/html');
-
-    return parsed.body.textContent || '';
+export function striphtml(html, exceptions = false) {
+    if(!exceptions) {
+        const parsed = new DOMParser().parseFromString(html, 'text/html');
+        return parsed.body.textContent || '';
+    } else {
+        const reg = new RegExp(`(<\/?(?![${exceptions}])[a-z]+(?=[\s>])(?:[^>=]|=(?:'[^']*'|"[^"]*"|[^'"\s]*))*\s?\/?>)`, 'gi')
+        return html.replace(reg, "");
+    }
 }
